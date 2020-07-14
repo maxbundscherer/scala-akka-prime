295bccb92f8016b0e4b9af5b1d8e621f082bdbc3
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-07-14 12:38:24 +0200

    improve doc

--- src/main/scala/de/maxbundscherer/akka/scala/prim/examples/MainExample.scala
+++ src/main/scala/de/maxbundscherer/akka/scala/prim/examples/MainExample.scala
@@ -6,16 +6,17 @@ import akka.actor.typed.ActorSystem
 
 object TestActor {
 
+  //Declare request wrapper and internal state
   sealed trait Request
-
   private case class State(accountBalance: Int)
 
+  //Declare concrete request
   case class IncreaseBalance(amount: Int) extends Request
 
-  //Default state is idle
+  //Default state is idle (define internal state)
   def apply(): Behavior[Request] = applyIdle(State(accountBalance = 0))
 
-  //State Idle (process messages)
+  //Process messages in state idle
   private def applyIdle(state: State): Behavior[Request] = Behaviors.receive { (context, message) =>
 
     message match {
@@ -25,7 +26,6 @@ object TestActor {
         val newState: State = state.copy(accountBalance = state.accountBalance + cmd.amount)
         context.log.info(s"Old balance was (${state.accountBalance}) / New balance is (${newState.accountBalance})")
         applyIdle(newState)
-
     }
 
   }
@@ -34,10 +34,10 @@ object TestActor {
 
 object MainExample extends App {
 
-  //Init actor system
+  //Init actor system and actor
   private val actorSystem: ActorSystem[TestActor.Request]  = ActorSystem(TestActor(), "actorSystem")
 
-  //Fire and forget
+  //Fire and forget two requests
   actorSystem ! TestActor.IncreaseBalance(amount = 10)
   actorSystem ! TestActor.IncreaseBalance(amount = -5)
 

824c989d156c90bb67253357737386f7e6f76c49
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-07-16 13:56:02 +0200

    Improve csv

--- src/main/scala/de/maxbundscherer/akka/scala/prim/utils/CSV.scala
+++ src/main/scala/de/maxbundscherer/akka/scala/prim/utils/CSV.scala
@@ -4,11 +4,11 @@ trait CSV {
 
   object CSVWriter {
 
-    def writeToCSV(to: Int, primeSize: Int, startTime: Long, time: Long, maxWorkers: Int, filename: String): Unit = {
+    def writeToCSV(to: Int, primeSize: Int, startTime: Long, maxWorkers: Int, time: Long, filename: String): Unit = {
 
       val writer = com.github.tototoshi.csv.CSVWriter.open(filename, append = true)
 
-      writer.writeRow(List(to, primeSize, startTime, time, maxWorkers))
+      writer.writeRow(List(to, primeSize, startTime, maxWorkers, time))
 
       writer.close()
 

1c2f0afb427b5fdda4324653f891d6cf76c020ff
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-07-16 17:38:01 +0200

    Improve to calc

--- src/main/scala/de/maxbundscherer/akka/scala/prim/Main.scala
+++ src/main/scala/de/maxbundscherer/akka/scala/prim/Main.scala
@@ -2,8 +2,8 @@ package de.maxbundscherer.akka.scala.prim
 
 object Main extends App {
 
-  import de.maxbundscherer.akka.scala.prim.utils.Calculator
   import de.maxbundscherer.akka.scala.prim.services.ActorSystemService
+  import de.maxbundscherer.akka.scala.prim.utils.LCMCalculator
   import de.maxbundscherer.akka.scala.prim.utils.JobController
 
   private val actorSystemService = new ActorSystemService()
@@ -13,7 +13,7 @@ object Main extends App {
   private val repeatRun: Int                = 5
   private val maxWorkersPerRun: Vector[Int] = Vector(1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22)
 
-  private val to = Calculator.calcTo(maxWorkersPerRun)
+  private val to: Int = LCMCalculator.lcmOfArray(maxWorkersPerRun.toArray).toInt
 
   JobController.startJob(
     actorSystemService  = actorSystemService,

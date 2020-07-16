0b6b179acba2ec07a352c874faeb2b1667ac05fc
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-07-16 17:38:40 +0200

    Mark old stuff as deprecated

--- src/main/scala/de/maxbundscherer/akka/scala/prim/utils/Calculator.scala
+++ src/main/scala/de/maxbundscherer/akka/scala/prim/utils/Calculator.scala
@@ -7,6 +7,7 @@ object Calculator {
    * @param values e.g. 1,2,3
    * @return e.g. 1 * 2 * 3
    */
+  @Deprecated
   def calcTo(values: Vector[Int]): Int = {
     var result = 1
     values.foreach(value => result = result * value)
